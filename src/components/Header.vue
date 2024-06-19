d1d9d6f1c4ac723ec779ccc4b2588eaa0c212c04
Benedettopana <benedetto.pana@gmail.com>
2024-06-19 13:10:54 +0200

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,63 @@
+<script>
+export default {
+  components: {},
+  data() {
+    return {};
+  },
+};
+</script>
+
+<template>
+  <header class="container-fluid">
+    <!--? Navbar -->
+    <nav class="navbar bg-primary fixed-top" data-bs-theme="dark">
+      <div class="container-fluid">
+        <a class="navbar-brand">Navbar</a>
+        <form class="d-flex" role="search">
+          <input
+            class="form-control me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
+    <!--? /Navbar -->
+    <!--% Jumbotron -->
+    <div class="container-jumbo w-100">
+      <div class="jumbo-img w-100">
+        <img :src="`../public/img/jumbotron/jumboexemple.jpg`" alt="" />
+      </div>
+    </div>
+    <!--% /Jumbotron -->
+  </header>
+</template>
+
+<style lang="scss" scoped>
+@use "../assets/scss/partials/general" as *;
+@use "../assets/scss/partials/variables" as *;
+
+header {
+  width: 99vw !important;
+
+  nav {
+    height: 70px;
+  }
+  .container-jumbo {
+    margin-top: 70px;
+    height: calc(99vh - 55px);
+    // height: 100%;
+    .jumbo-img {
+      // width: 100%;
+      height: 100%;
+      img {
+        height: 100%;
+        // width: 100%;
+        object-fit: cover;
+      }
+    }
+  }
+}
+</style>
