40e2239acc4663af798eb23aa0e741d99396a040
Benedettopana <benedetto.pana@gmail.com>
2024-06-20 08:45:50 +0200

    [fix]SCSS

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -21,6 +21,6 @@ export default {
 </template>
 
 <style lang="scss" scoped>
-@use "../../assets/scss/partials/general" as *;
-@use "../../assets/scss/partials/variables" as *;
+@use "../assets/scss/partials/general" as *;
+@use "../assets/scss/partials/variables" as *;
 </style>
