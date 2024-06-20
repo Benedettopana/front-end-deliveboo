e9f5859355edb8d8b24d64f20899e4882b110c7e
Benedettopana <benedetto.pana@gmail.com>
2024-06-20 11:03:29 +0200

    [add]Restaurant-menu>dish-card

--- src/pages/RestaurantMenu.vue
+++ src/pages/RestaurantMenu.vue
@@ -1,19 +1,49 @@
 <script>
-  export default {
-    name:'restaurantMenu'
-  }
+import FoodCard from "../components/partials/FoodCard.vue";
+export default {
+  name: "restaurantMenu",
+
+  components: {
+    FoodCard,
+  },
+};
 </script>
 
-
-
 <template>
-  <div>
-    <h1 class="mt-5">Menu del ristorante</h1>
+  <div class="container-fluid">
+    <!-- TODO:Aggiungere  in modo dinamico il nome -->
+    <h1 class="text-center mt-5">Il mio ristorante</h1>
+
+    <div class="row row-cols-3 my-5">
+      <div class="col-2">aside</div>
+      <div class="col-8">
+        <!--? Card piatti -->
+        <div class="row">
+          <!-- TODO: Stampa card dinamiche -->
+          <div class="col-12">
+            <FoodCard />
+          </div>
+          <div class="col-12">
+            <FoodCard />
+          </div>
+          <div class="col-12">
+            <FoodCard />
+          </div>
+          <div class="col-12">
+            <FoodCard />
+          </div>
+          <div class="col-12">
+            <FoodCard />
+          </div>
+          <div class="col-12">
+            <FoodCard />
+          </div>
+        </div>
+        <!--? /Card piatti -->
+      </div>
+      <div class="col-2">check</div>
+    </div>
   </div>
 </template>
 
-
-
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
