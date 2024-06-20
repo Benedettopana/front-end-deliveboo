8cf979a2e9294dd78f7f970f9fe55e219939c80d
Benedettopana <benedetto.pana@gmail.com>
2024-06-20 09:39:03 +0200

    [add]restaurant card

--- src/components/AdvancedSearch-partials/Restaurant.vue
+++ src/components/AdvancedSearch-partials/Restaurant.vue
@@ -1,9 +1,18 @@
 <script>
-export default {};
+import RestaurantCard from "../partials/RestaurantCard.vue";
+export default {
+  components: {
+    RestaurantCard,
+  },
+};
 </script>
 <template>
   <div>
     <h1>Restaurant</h1>
+
+    <RestaurantCard />
+    <RestaurantCard />
+    <RestaurantCard />
   </div>
 </template>
 
---
+++ src/components/partials/RestaurantCard.vue
@@ -0,0 +1,46 @@
+<script>
+export default {};
+</script>
+<template>
+  <div class="col-12 my-5">
+    <div class="restaurant-card">
+      <!--! restaurant img -->
+      <div class="restaurant-tumb mb-3">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque,
+        temporibus. Facere numquam placeat dignissimos quod deleniti nesciunt
+        ipsum quibusdam, iure quia molestias possimus porro tenetur, fugit sit
+        saepe itaque eveniet!
+      </div>
+      <!--! /restaurant img -->
+      <!--? restaurant info -->
+      <div class="restaurant-info">
+        <p>Nome: Il mio ristorant</p>
+        <p>Categorie: pizza</p>
+        <p>Indirizzo: Via dei platani 12</p>
+        <p>
+          Descrizione: Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Tempora quaerat recusandae eveniet sit velit beatae illo architecto
+          magni! Animi hic ex soluta dignissimos reprehenderit, corrupti
+          voluptatem possimus molestiae unde dolor commodi nostrum, accusamus
+          expedita dicta harum vero, aut est assumenda.
+        </p>
+      </div>
+      <!--? /restaurant info -->
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use "../../assets/scss/partials/general" as *;
+@use "../../assets/scss/partials/variables" as *;
+
+.restaurant-card {
+  width: 80%;
+  .restaurant-tumb {
+    background-color: #210b2d;
+    width: 100%;
+    height: 150px;
+    border-radius: 20px;
+  }
+}
+</style>
