eb23df0caed877c920fc433caa24a651ea77d735
ElenaRuscitto <ruscitto.elena@gmail.com>
2024-06-20 11:29:03 +0200

    [add] app.vue axios

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 import Header from "./components/Header.vue";
 import Footer from "./components/Footer.vue";
 import axios from "axios";
-// import {store} from './data/store';
+import {store} from './data/store';
 
 export default {
   components: {
@@ -14,8 +14,31 @@ export default {
   data() {
     return {
       axios,
+      store,
+      restaurants: [],
+      // typeName: [],
     };
   },
+
+    methods: {
+      getApi(){
+        axios.get(store.apiUrl + '/restaurants')
+        .then(result=> {
+          // this.typeName = result.data.types.name;
+          this.restaurants = result.data.restaurants
+          
+          console.log(result.data);
+          console.log(this.typeName);
+        })
+        .catch(error => {
+        console.log(error);
+        console.log(error.message);
+        })
+      }
+    },
+    mounted(){
+      this.getApi(this.$route.params)
+    }
 };
 </script>
 
