ec750f89096b757b8cf0f3ffbdfc19e223fae8c1
Benedettopana <benedetto.pana@gmail.com>
2024-06-22 15:17:38 +0200

    [add]Salvataggio/caricamento carrello

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,29 @@
 import { createStore } from "vuex";
 
+//? Salvataggio dei dati nel localStorage
+const saveCartToLocalStorage = (cart) => {
+  localStorage.setItem("cart", JSON.stringify(cart));
+};
+
+//? Caricamento dei dati dal localStorage
+const loadCartFromLocalStorage = () => {
+  const cart = localStorage.getItem("cart");
+  return cart ? JSON.parse(cart) : [];
+};
+
 export const store = createStore({
   state: {
-    cart: [], // Array per contenere i piatti nel carrello
+    cart: loadCartFromLocalStorage(),
+    // cart: [], // Array per contenere i piatti nel carrello
   },
   mutations: {
     addToCart(state, dish) {
       state.cart.push(dish);
+      saveCartToLocalStorage(state.cart);
     },
     removeFromCart(state, index) {
       state.cart.splice(index, 1);
+      saveCartToLocalStorage(state.cart);
     },
   },
   actions: {
